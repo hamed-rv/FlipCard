a93f1f643a608d23b5f85527c779a73098d4e717
h.rahimvand <h.rahimvand@roundtableapps.com>
2020-02-22 11:10:35 +0330

    - Add BaseActivity - Separate FlipCardActivity - Create ViewPagerFlipCard feature

--- app/src/main/java/com/hamedrahimvand/flipcard/simple_flip_card/FlipCardActivity.kt
+++ app/src/main/java/com/hamedrahimvand/flipcard/simple_flip_card/FlipCardActivity.kt
@@ -1,18 +1,21 @@
-package com.hamedrahimvand.flipcard
+package com.hamedrahimvand.flipcard.simple_flip_card
 
 import android.animation.Animator
 import android.animation.AnimatorInflater
 import android.animation.AnimatorSet
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import com.hamedrahimvand.flipcard.R
+import com.hamedrahimvand.flipcard.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_flip_card.*
 
 /**
  *@author Hamed.Rahimvand
  *@since 2020-02-22
+ * Basic implementation of FlipCard with clockwise and counter-clockwise modes.
  */
-class MainActivity : AppCompatActivity() {
+class FlipCardActivity : BaseActivity() {
 
+    override fun layoutId(): Int = R.layout.activity_flip_card
     //CounterClockWise
     private lateinit var animatorSetLeftIn: AnimatorSet
     private lateinit var animatorSetRightOut: AnimatorSet
@@ -25,7 +28,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         loadAnimations()
         changeCameraDistance()
         ibFlip.setOnClickListener {
@@ -34,17 +36,29 @@ class MainActivity : AppCompatActivity() {
         ibFlipClockWise.setOnClickListener {
             flipClockWise()
         }
-        ibFlipCounterClockWise.setOnClickListener{
+        ibFlipCounterClockWise.setOnClickListener {
             flipCounterClockWise()
         }
     }
 
     private fun loadAnimations() {
-        animatorSetLeftIn = AnimatorInflater.loadAnimator(this, R.animator.left_in_anim) as AnimatorSet
-        animatorSetRightOut = AnimatorInflater.loadAnimator(this, R.animator.right_out_anim) as AnimatorSet
+        animatorSetLeftIn = AnimatorInflater.loadAnimator(
+            this,
+            R.animator.left_in_anim
+        ) as AnimatorSet
+        animatorSetRightOut = AnimatorInflater.loadAnimator(
+            this,
+            R.animator.right_out_anim
+        ) as AnimatorSet
 
-        animatorSetLeftOut = AnimatorInflater.loadAnimator(this, R.animator.left_out_anim) as AnimatorSet
-        animatorSetRightIn = AnimatorInflater.loadAnimator(this, R.animator.right_in_anim) as AnimatorSet
+        animatorSetLeftOut = AnimatorInflater.loadAnimator(
+            this,
+            R.animator.left_out_anim
+        ) as AnimatorSet
+        animatorSetRightIn = AnimatorInflater.loadAnimator(
+            this,
+            R.animator.right_in_anim
+        ) as AnimatorSet
 
         animatorSetLeftIn.addListener(object : Animator.AnimatorListener {
             override fun onAnimationRepeat(animation: Animator?) {
@@ -95,7 +109,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun flipClockWise(){
+    private fun flipClockWise() {
         if (!canFlip) return
         if (!isBackVisible) {
             animatorSetLeftOut.setTarget(cardFront)
@@ -108,7 +122,8 @@ class MainActivity : AppCompatActivity() {
         animatorSetRightIn.start()
         isBackVisible = !isBackVisible
     }
-    private fun flipCounterClockWise(){
+
+    private fun flipCounterClockWise() {
         if (!canFlip) return
         if (!isBackVisible) {
             animatorSetRightOut.setTarget(cardFront)
