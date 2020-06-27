e304243ea2a6d9a896807f3e7f1798d4d7e5ab28
h.rahimvand <h.rahimvand@roundtableapps.com>
2020-06-27 10:01:44 +0430

    Fix transform scale. change page alpha

--- app/src/main/java/com/hamedrahimvand/flipcard/viewpager_flip_card/FlipCardViewPager.kt
+++ app/src/main/java/com/hamedrahimvand/flipcard/viewpager_flip_card/FlipCardViewPager.kt
@@ -6,6 +6,7 @@ import android.view.View
 import androidx.viewpager.widget.ViewPager
 import com.hamedrahimvand.flipcard.utils.GlobalFunctions
 import kotlin.math.abs
+import kotlin.math.min
 
 
 /**
@@ -25,24 +26,23 @@ class FlipCardViewPager(context: Context, attributeSet: AttributeSet?) :
     }
 
     override fun transformPage(page: View, position: Float) {
-        val absolutePosition = abs(position)
+        val absolutePosition = abs(position) / 5
         if (position <= -1.0f || position >= 1.0f) {
             // Page is not visible -- stop any running animations
         } else if (position == 0.0f) {
             // Page is selected -- reset any views if necessary
-            page.scaleX = (1).toFloat()
-            page.scaleY = (1).toFloat()
+            page.scaleX = 1f
+            page.scaleY = 1f
+            page.alpha = 1f
         } else {
-            page.scaleY = if (absolutePosition < 0.1) {
-                (1 - absolutePosition)
-            } else {
-                1 - 0.1f
-            }
-            page.scaleX = if (absolutePosition < 0.1) {
-                (1 - absolutePosition)
-            } else {
-                1 - 0.1f
-            }
+            page.scaleY = 1 - min(absolutePosition,0.2f)
+//            page.scaleX = 1 - min(absolutePosition,0.1f)
+
+//            page.alpha = if (abs(position) < 0.3) {
+//                (1 - abs(position))
+//            } else {
+//                1 - 0.3f
+//            }
         }
     }
 
